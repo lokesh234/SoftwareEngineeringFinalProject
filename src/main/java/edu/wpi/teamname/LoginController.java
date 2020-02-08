9cfadfcc5968515f3ee9de1ff5b2462f56d0ea8f
jetaehyun <jetaehyun0@gmail.com>
2020-02-08 15:10:48 -0500

    add bare code for loginController

---
+++ src/main/java/edu/wpi/teamname/LoginController.java
@@ -0,0 +1,33 @@
+package edu.wpi.teamname;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class LoginController {
+
+    @FXML private TextField user;
+    @FXML private TextField pass;
+
+//    private String userCheck;
+//    private String passCheck;
+
+    /**
+     *
+     * @param user
+     * @param pass
+     */
+    private void sendLogin(String user, String pass) {
+
+    }
+
+
+    /**
+     * checks whether is the user entered the correct credentials
+     * @return false if incorrect, true if correct
+     */
+    private boolean isAuthorized() {
+        return false;
+    }
+
+}
