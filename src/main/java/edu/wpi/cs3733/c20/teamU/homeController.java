75ba4694b0620483d40151ef7107e1d37164e99f
jetaehyun <jetaehyun0@gmail.com>
2020-02-09 14:10:41 -0500

    added homeController

---
+++ src/main/java/edu/wpi/cs3733/c20/teamU/homeController.java
@@ -0,0 +1,35 @@
+package edu.wpi.cs3733.c20.teamU;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Popup;
+
+public class homeController {
+  @FXML private Button test;
+  private Parent parent;
+  private Parent root;
+  private Popup popup;
+
+
+  public void setParent(Parent parent, Parent root, Popup popup) {
+    this.parent = parent;
+    this.popup = popup;
+    this.root = root;
+  }
+
+  @FXML
+  private void advanceScene(ActionEvent e) {
+    if(!popup.isShowing()) {
+      root.setOpacity(.5);
+//      root.setDisable(true); TODO: set appropriate disable system
+      popup.show(App.getPrimaryStage());
+    }
+    else popup.hide();
+//    App.getPrimaryStage().getScene().setRoot(parent);
+  }
+
+
+
+}
