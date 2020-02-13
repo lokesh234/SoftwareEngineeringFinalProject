38b6d0877c407dd580beae6b346efb399f6fd01a
lokesh234 <42650455+lokesh234@users.noreply.github.com>
2020-02-13 15:39:36 -0500

    Changed Icon location to png_files

--- src/main/java/edu/wpi/cs3733/c20/teamU/App.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/App.java
@@ -359,7 +359,7 @@ public class App extends Application {
 //      deleteNodeScene = new Scene(deleteNode);
 
       primaryStage.setScene(startScene);
-      primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/pharmacy.png")));
+      primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/png_files/pharmacy.png")));
       primaryStage.show();
     } catch (IOException e) {
       e.printStackTrace();
