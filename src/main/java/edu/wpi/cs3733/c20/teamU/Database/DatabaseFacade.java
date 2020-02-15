88240fa7a973128079fd45b30a820c4c6bfc519b
Cannotheat <bo23519@gmail.com>
2020-02-15 01:04:10 -0500

    Write templates for all the facade

---
+++ src/main/java/edu/wpi/cs3733/c20/teamU/Database/DatabaseFacade.java
@@ -0,0 +1,45 @@
+package edu.wpi.cs3733.c20.teamU.Database;
+
+import edu.wpi.cs3733.c20.teamU.ServiceRequest.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DatabaseFacade {
+    public void getNodes(HashMap<String,Node> nhm){
+        return;
+    }
+    public void setNode(Node node){
+        return;
+    }
+    public void rmNode(Node node){
+        return;
+    }
+
+    public void getEdges(HashMap<String,Edge> ehm, HashMap<String,Node> nhm){
+        return;
+    }
+    public void setEdge(Edge edge){
+        return;
+    }
+    public void rmEdge(Edge edge){
+        return;
+    }
+
+
+    public void getRequests(ArrayList<Service> services){
+        return;
+    }
+
+    public void addRequest(Service service){
+        return;;
+    }
+
+    public void resolveRequest(Service service){
+        return;
+    }
+
+    public boolean login(String user, String pass){
+        return true;
+    }
+}
---
+++ src/main/java/edu/wpi/cs3733/c20/teamU/Navigation/NavigationFacade.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs3733.c20.teamU.Navigation;
+
+import edu.wpi.cs3733.c20.teamU.Database.Edge;
+import edu.wpi.cs3733.c20.teamU.Database.Node;
+
+import java.util.ArrayList;
+
+public class NavigationFacade {
+    public ArrayList<Edge> getPath(Node node1, Node node2) {
+        return null;
+    }
+
+    public void setSearchType(String searchType) {
+        return;
+    }
+}
