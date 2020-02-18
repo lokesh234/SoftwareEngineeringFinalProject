a3ab6d41498d7597a3dc5324a99454b8b52a9808
lokesh234 <42650455+lokesh234@users.noreply.github.com>
2020-02-18 17:46:31 -0500

    Hello

--- src/main/java/edu/wpi/cs3733/c20/teamU/Navigation/PathfindTextController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/Navigation/PathfindTextController.java
@@ -3,11 +3,26 @@ package edu.wpi.cs3733.c20.teamU.Navigation;
 import edu.wpi.cs3733.c20.teamU.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class PathfindTextController {
+
+    @FXML Label Directions;
+
     @FXML
     public void exitPopup(ActionEvent e){
         App.getTextDirectionsPop().getContent().remove(0);
+        //Directions.setText("YEEHAW");
     }
 
+    public void Populate(){
+        for (int i = 0; i < App.getTextpath().size(); i++){
+            System.out.println(Directions);
+            Directions.setText(App.getTextpath().get(i));
+        }
+    }
+
+    public void showDirections(){
+        Populate();
+    }
 }
