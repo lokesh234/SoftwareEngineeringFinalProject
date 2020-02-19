3bc4f2b61a687e2a5fc96942955c6ff833cd8b77
ajlewis02 <ajlewis@wpi.edu>
2020-02-19 04:40:44 -0500

    NodeType field in add node now combo box

--- src/main/java/edu/wpi/cs3733/c20/teamU/Administration/AddNodeScreenController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/Administration/AddNodeScreenController.java
@@ -1,12 +1,17 @@
 package edu.wpi.cs3733.c20.teamU.Administration;
 
+import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.cs3733.c20.teamU.App;
 import edu.wpi.cs3733.c20.teamU.Database.Database;
 import edu.wpi.cs3733.c20.teamU.Database.DatabaseWrapper;
 import edu.wpi.cs3733.c20.teamU.Database.Node;
+import javafx.collections.FXCollections;
+import javafx.collections.FXCollections.*;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -19,7 +24,8 @@ public class AddNodeScreenController {
   @FXML
   private Button cancel;
   @FXML
-  private TextField text1, text2, text3, text4, text5, text6, text7, text8;
+  private TextField text1, text2, text3, text4, text5, text7, text8;
+  @FXML private JFXComboBox nodeType;
   private String ID, x, y, floor, build, type, shortName, longName;
   //  Node selectedNode;
   GraphEditController nodeViewScreenController;
@@ -46,14 +52,17 @@ public class AddNodeScreenController {
     text3.clear();
     text4.clear();
     text5.clear();
-    text6.clear();
     text7.clear();
     text8.clear();
   }
 
   @FXML
   private void confirmChange() {
-    overWrite();
+    try {
+      overWrite();
+    } catch (Exception e) {
+      return; //If this fails, there's a problem with the text input
+    }
     HashMap<String, Node> graph = new HashMap<String, Node>();
     Database.getNodes(graph);
 //    System.out.println(graph.get(userID).getFloor());
@@ -70,7 +79,7 @@ public class AddNodeScreenController {
     String userY = text3.getText();
     String userFloor = text4.getText();
     String userBuild = text5.getText();
-    String userType = text6.getText();
+    String userType = nodeType.getValue().toString();
     String userLongName = text7.getText();
     String userShortName = text8.getText();
 //    System.out.println(userID);
@@ -78,9 +87,10 @@ public class AddNodeScreenController {
     if(userX.isEmpty()) userX = x;
     if(userY.isEmpty()) userY = y;
 
-     if (!DatabaseWrapper.addNode(userID, (int) Double.parseDouble(userX), (int) Double.parseDouble(userY),
-          (int) Double.parseDouble(userFloor), userBuild, userType, userLongName,
-          userShortName)) System.out.println("oh no 2"); // THIS IS FAILING
+      if (!DatabaseWrapper.addNode(userID, (int) Double.parseDouble(userX), (int) Double.parseDouble(userY),
+              (int) Double.parseDouble(userFloor), userBuild, userType, userLongName,
+              userShortName)) System.out.println("oh no 2"); // THIS IS FAILING
+
       //System.out.println("something");
 
 //    if (boo) {
@@ -93,7 +103,6 @@ public class AddNodeScreenController {
     text3.clear();
     text4.clear();
     text5.clear();
-    text6.clear();
     text7.clear();
     text8.clear();
     cancel.fire();
@@ -112,5 +121,8 @@ public class AddNodeScreenController {
 
   @FXML
   public void initialize() {
+    ObservableList<String> typeOptions = FXCollections.observableArrayList("DEPT","HALL","CONF","REST","STAI","ELEV","LABS","INFO","CONF","EXIT","RETL","SERV");
+    nodeType.getItems().addAll(typeOptions);
+    nodeType.setValue("HALL");
   }
 }
