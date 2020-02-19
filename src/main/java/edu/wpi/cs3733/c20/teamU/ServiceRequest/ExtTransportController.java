e80140e8a3c67bc4d769aa96e2267f1b86e2e80f
jetaehyun <jetaehyun0@gmail.com>
2020-02-19 08:04:47 -0500

    fixed nullptr issue in ExtTransport

--- src/main/java/edu/wpi/cs3733/c20/teamU/ServiceRequest/ExtTransportController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/ServiceRequest/ExtTransportController.java
@@ -14,66 +14,67 @@ import javafx.fxml.FXML;
 
 public class ExtTransportController {
 
-        @FXML private JFXTextField firstNameText;
-        @FXML private JFXTextField lastNameText;
-        @FXML private JFXTextField passwordText;
-        @FXML private JFXTextField confirmPassText;
-        @FXML private JFXTimePicker tp;
-        @FXML private JFXDatePicker dp;
-        @FXML private JFXButton cancel;
-        @FXML private JFXButton confirm;
+  @FXML private JFXTextField firstNameText;
+  @FXML private JFXTextField lastNameText;
+  @FXML private JFXTextField passwordText;
+  @FXML private JFXTextField confirmPassText;
+  @FXML private JFXTimePicker tp;
+  @FXML private JFXDatePicker dp;
+  @FXML private JFXButton cancel;
+  @FXML private JFXButton confirm;
 
-        @FXML
-        private void getSubmission() {
-            String userLast = lastNameText.getText();
-            String userFirst = firstNameText.getText();
-            String dest = passwordText.getText();
-            String numOfPas = confirmPassText.getText();
-            String t = tp.getValue().toString();
-            String d = dp.getValue().toString();
+  @FXML
+  private void getSubmission() {
+    String userLast = lastNameText.getText();
+    String userFirst = firstNameText.getText();
+    String dest = passwordText.getText();
+    String numOfPas = confirmPassText.getText();
+    String t = tp.getValue().toString().trim();
+    String d = dp.getValue().toString().trim();
 
-            //TODO: specify which textfield to write in if empty
-            if(dest.isEmpty() || userLast.isEmpty() || userFirst.isEmpty() || numOfPas.isEmpty() || d.isEmpty() || t.isEmpty()) {
-                // will print out some text eventually, right now nothing
-                lastNameText.setStyle("-fx-border-color: red");
-                confirmPassText.setStyle("-fx-border-color: red");
-                firstNameText.setStyle("-fx-border-color: red");
-                dp.setStyle("-fx-border-color: red");
-                tp.setStyle("-fx-border-color: red");
-                passwordText.setStyle("-fx-border-color: red");
-            } else {
-                lastNameText.setStyle("-fx-border-color:  #FFEEC9");
-                confirmPassText.setStyle("-fx-border-color:  #FFEEC9");
-                firstNameText.setStyle("-fx-border-color:  #FFEEC9");
-                passwordText.setStyle("-fx-border-color:  #FFEEC9");
-               // DatabaseWrapper.medicineSRAdd(userFirst, userLast, userDrug, userFreq, userDelivery, userComment);
-                //ServiceDatabase.medicineSRAdd(userFirst, userLast, userDrug, userFreq, userDelivery, userComment);
-                DatabaseWrapper.extTransportSRAdd(userLast,userFirst,dest,t,d,Integer.getInteger(numOfPas));
-                clearField();
-                cancel.fire();
-            }
-        }
+    //TODO: specify which textfield to write in if empty
+    if (dest.isEmpty() || userLast.isEmpty() || userFirst.isEmpty() || numOfPas.isEmpty() || d
+        .isEmpty() || t.isEmpty()) {
+      // will print out some text eventually, right now nothing
+      lastNameText.setStyle("-fx-border-color: red");
+      confirmPassText.setStyle("-fx-border-color: red");
+      firstNameText.setStyle("-fx-border-color: red");
+      dp.setStyle("-fx-border-color: red");
+      tp.setStyle("-fx-border-color: red");
+      passwordText.setStyle("-fx-border-color: red");
+    } else {
+      lastNameText.setStyle("-fx-border-color:  #FFEEC9");
+      confirmPassText.setStyle("-fx-border-color:  #FFEEC9");
+      firstNameText.setStyle("-fx-border-color:  #FFEEC9");
+      passwordText.setStyle("-fx-border-color:  #FFEEC9");
+      DatabaseWrapper
+          .extTransportSRAdd(userLast, userFirst, dest, t, d, Integer.parseInt(numOfPas));
+      clearField();
+      cancel.fire();
+    }
+  }
 
-        public void clearField() {
-            lastNameText.clear();
-            firstNameText.clear();
-            passwordText.clear();
-            confirmPassText.clear();
-            dp.getEditor().clear();
-            tp.getEditor().clear();
-        }
+  public void clearField() {
+    lastNameText.clear();
+    firstNameText.clear();
+    passwordText.clear();
+    confirmPassText.clear();
+    dp.getEditor().clear();
+    tp.getEditor().clear();
+  }
 
-        @FXML
-        private void goBack(){
+  @FXML
+  private void goBack() {
 //        App.getHome().setOpacity(1);
 //        App.getHome().setDisable(false);
-            clearField();
-            App.getExtTransportPop().getContent().clear();
-            App.getRequestPop().getContent().add(App.getRequest());
-            App.getRequestPop().show(App.getPrimaryStage());
-        }
+//    clearField();
+    App.getExtTransportPop().getContent().clear();
+    App.getRequestPop().getContent().add(App.getRequest());
+    App.getRequestPop().show(App.getPrimaryStage());
+  }
 
-        @FXML
-        public void initialize() {}
+  @FXML
+  public void initialize() {
+  }
 
 }
