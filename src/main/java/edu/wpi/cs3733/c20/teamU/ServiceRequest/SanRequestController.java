225d099bb4488d23bdf6817162c8301423da5c8b
mtcharter <46787873+mtcharter@users.noreply.github.com>
2020-02-18 21:46:50 -0500

    san Request pt2

--- src/main/java/edu/wpi/cs3733/c20/teamU/ServiceRequest/SanRequestController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/ServiceRequest/SanRequestController.java
@@ -1,5 +1,19 @@
 package edu.wpi.cs3733.c20.teamU.ServiceRequest;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import edu.wpi.cs3733.c20.teamU.App;
+import edu.wpi.cs3733.c20.teamU.Database.ServiceDatabase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+
 public class SanRequestController {
 
+    @FXML private JFXTextArea service;
+    @FXML private JFXTextField location;
+    @FXML private JFXTextField nature;
+    @FXML private JFXTextArea additional;
+    @FXML private JFXButton submit;
 }
