54a3e9c7baa416bed28e0c7c5a49d2ef5d013475
lokesh234 <42650455+lokesh234@users.noreply.github.com>
2020-02-19 01:17:14 -0500

    Added more functionality for EXTransport

--- src/main/java/edu/wpi/cs3733/c20/teamU/ServiceRequest/ExtTransportController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/ServiceRequest/ExtTransportController.java
@@ -1,10 +1,16 @@
 package edu.wpi.cs3733.c20.teamU.ServiceRequest;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.c20.teamU.App;
+import edu.wpi.cs3733.c20.teamU.Database.DatabaseWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class ExtTransportController {
+
+    @FXML private JFXTextField firstNameText, lastNameText, passwordText, confirmPassText;
+    @FXML private JFXButton confirm;
 
     @FXML
     public void cancelExt(ActionEvent e){
@@ -13,6 +19,38 @@ public class ExtTransportController {
         App.getHome().setOpacity(.5);
         App.getHome().setDisable(true);
         App.getRequestPop().show(App.getPrimaryStage());
+    }
+
+    @FXML
+    private void getSubmission() {
+        String userLast = lastNameText.getText();
+        String userpassword = passwordText.getText();
+        String userFirst = firstNameText.getText();
+        String userConfirm = confirmPassText.getText();
+
+        //TODO: specify which textfield to write in if empty
+        if(userLast.isEmpty() || userFirst.isEmpty() || userConfirm.isEmpty() || userpassword.isEmpty()) {
+            // will print out some text eventually, right now nothing
+            firstNameText.setStyle("-fx-border-color: red");
+            passwordText.setStyle("-fx-border-color: red");
+            lastNameText.setStyle("-fx-border-color: red");
+            confirmPassText.setStyle("-fx-border-color: red");
+        } else {
+            firstNameText.setStyle("-fx-border-color:  #FFEEC9");
+            lastNameText.setStyle("-fx-border-color:  #FFEEC9");
+            passwordText.setStyle("-fx-border-color:  #FFEEC9");
+            confirmPassText.setStyle("-fx-border-color:  #FFEEC9");
+            //ServiceDatabase.medicineSRAdd(userFirst, userLast, userDrug, userFreq, userDelivery, userComment);
+            clearField();
+            confirm.fire();
+        }
+    }
+
+    public void clearField() {
+        firstNameText.clear();
+        lastNameText.clear();
+        passwordText.clear();
+        confirmPassText.clear();
     }
 
 }
