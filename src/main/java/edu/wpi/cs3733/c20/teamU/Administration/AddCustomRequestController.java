c5b741b98ede54d454706416d0ba368c796c64ed
ajlewis02 <ajlewis@wpi.edu>
2020-03-04 04:22:50 -0500

    Merge branch 'clownArmy' into dev
    # Conflicts:
    #	src/main/java/edu/wpi/cs3733/c20/teamU/Navigation/PathfindController.java

--- src/main/java/edu/wpi/cs3733/c20/teamU/Administration/AddCustomRequestController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamU/Administration/AddCustomRequestController.java
@@ -172,7 +172,7 @@ public class AddCustomRequestController {
     private boolean isValidComp(String name, String type, Collection<String> contents) {
         if ((type.equals("Combo Box") || type.equals("Radio Buttons")) && contents.size() == 0) return false;
         if (name.contains(":") || type.contains(":")) return false;
-        if (components.getItems().size() > 5) return false; //Size limit!
+        if (components.getItems().size() >= 5) return false; //Size limit!
         for (String s : contents) {
             if (s.contains(",")) return false;
         }
